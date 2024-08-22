1926039a133d8ea287d468d64d86702113715d64
Arun <arun@siara.cc>
2024-08-22 17:55:42 +0530

    ptr 64 bits

--- src/bv.hpp
+++ src/bv.hpp
@@ -59,40 +59,53 @@ class int_bit_vector {
     int count;
     int last_idx;
     uint8_t last_byte_bits;
+    static void append_uint32(uint32_t u32, byte_vec& v) {
+      v.push_back(u32 & 0xFF);
+      v.push_back((u32 >> 8) & 0xFF);
+      v.push_back((u32 >> 16) & 0xFF);
+      v.push_back(u32 >> 24);
+    }
+    static void append_uint64(uint64_t u64, byte_vec& v) {
+      v.push_back(u64 & 0xFF);
+      v.push_back((u64 >> 8) & 0xFF);
+      v.push_back((u64 >> 16) & 0xFF);
+      v.push_back((u64 >> 24) & 0xFF);
+      v.push_back((u64 >> 32) & 0xFF);
+      v.push_back((u64 >> 40) & 0xFF);
+      v.push_back((u64 >> 48) & 0xFF);
+      v.push_back(u64 >> 56);
+    }
   public:
     int_bit_vector() {
     }
-    int_bit_vector(byte_vec *_ptrs, int _bit_len, int _count)
-        : int_ptrs (_ptrs), bit_len (_bit_len), count (_count) {
-      int_ptrs->clear();
-      int_ptrs->reserve(bit_len * count / 8 + 1);
-      int_ptrs->push_back(0);
-      last_byte_bits = 8;
-      last_idx = 0;
+    int_bit_vector(byte_vec *_ptrs, int _bit_len, int _count) {
+      init(_ptrs, _bit_len, _count);
     }
     void init(byte_vec *_ptrs, int _bit_len, int _count) {
       int_ptrs = _ptrs;
       bit_len = _bit_len;
       count = _count;
       int_ptrs->clear();
-      int_ptrs->reserve(bit_len * count / 8 + 1);
-      int_ptrs->push_back(0);
-      last_byte_bits = 8;
+      int_ptrs->reserve(bit_len * count / 8 + 8);
+      append_uint64(0, *int_ptrs);
+      last_byte_bits = 64;
       last_idx = 0;
     }
-    void append(uint32_t ptr) {
+    void append(uint32_t given_ptr) {
+      uint64_t ptr = given_ptr;
+      uint64_t *last_ptr = (uint64_t *) (int_ptrs->data() + int_ptrs->size() - 8);
       int bits_to_append = bit_len;
       while (bits_to_append > 0) {
         if (bits_to_append < last_byte_bits) {
           last_byte_bits -= bits_to_append;
-          (*int_ptrs)[last_idx] |= (ptr << last_byte_bits);
+          *last_ptr |= (ptr << last_byte_bits);
           bits_to_append = 0;
         } else {
           bits_to_append -= last_byte_bits;
-          (*int_ptrs)[last_idx] |= (ptr >> bits_to_append);
-          int_ptrs->push_back(0);
-          last_byte_bits = 8;
-          last_idx++;
+          *last_ptr |= (ptr >> bits_to_append);
+          last_byte_bits = 64;
+          append_uint64(0, *int_ptrs);
+          last_ptr = (uint64_t *) (int_ptrs->data() + int_ptrs->size() - 8);
         }
       }
     }
@@ -110,18 +123,13 @@ class int_bv_reader {
       bit_len = _bit_len;
     }
     uint32_t operator[](int pos) {
-      uint32_t bit_pos = pos * bit_len;
-      uint32_t byte_pos = bit_pos / 8;
-      uint8_t bits_occu = (bit_pos % 8);
-      const uint8_t *loc = int_bv + byte_pos;
-      uint32_t ret = *loc++ & (0xFF >> bits_occu);
-      int bits_left = bit_len + (bits_occu - 8);
-      while (bits_left > 0) {
-        ret = (ret << 8) | *loc++;
-        bits_left -= 8;
-      }
-      ret >>= (bits_left * -1);
-      return ret;
+      uint64_t bit_pos = pos * bit_len;
+      uint64_t *ptr_loc = (uint64_t *) int_bv + bit_pos / 64;
+      int bits_occu = (bit_pos % 64);
+      uint64_t ret = (*ptr_loc++ << bits_occu);
+      if (bits_occu + bit_len <= 64)
+        return ret >> (64 - bit_len);
+      return (ret | (*ptr_loc >> (64 - bits_occu))) >> (64 - bit_len);
     }
 };
 
